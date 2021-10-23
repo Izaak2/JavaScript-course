08bef1302bb88dcfaeb529ac6ab57d32cebda7c8
Izaak2 <izaak.git@gmail.com>
2021-10-23 12:28:27 +0200

    tutorail updates

--- javascript/learn-to-code.js
+++ javascript/learn-to-code.js
@@ -66,56 +66,110 @@
 //for (var index = 0 ; index < studnets.length; index++) {
 //    console.log(studnets[index]);
 //}
+//
+//function area(lenght, width){
+//    return lenght * width;
+//}
+//
+//var area1 = area(10, 15);
+//console.log(area1);
+//
+//var rectangles = [];
+//rectangles.push(area(10,15));
+//rectangles.push(area(13,15));
+//rectangles.push(area(10,1));
+//
+//console.log(rectangles)
+//
+//
+//var bankBalance = 500;
+//
+//function makeTransaction(priceOfgSale) {
+//    if (priceOfgSale <= bankBalance){
+//        bankBalance -= priceOfgSale;
+//        console.log("Purchase successful")
+//    } else {
+//        console.log("insufficient funds")
+//    }
+//}
+//
+//
+//
+//console.log(bankBalance);
+//makeTransaction(79.00);
+//console.log(bankBalance);
+//makeTransaction(9.00);
+//console.log(bankBalance);
+//makeTransaction(49.00);
+//console.log(bankBalance);
+//makeTransaction(709.00);
+//console.log(bankBalance);
 
-function area(lenght, width){
-    return lenght * width;
+/* Object 
+    it is like a hashtable;
+*/
+
+
+//var student0 = {
+//    firstName: "John",
+//    lastName: "Smith",
+//    age: 6,
+//    greeting: function () {
+//        return "I am " + this.firstName + " and I am " + this.age + " years old.";
+//    }
+//    
+//};
+//
+//console.log(student0.greeting());
+//
+//// creates a new empty object
+//var student1 = new Object();
+//student1.firstName = "kacper";
+//student1.lastName = "wojtasik";
+//student1.age = 12;
+//
+//// Another way of creating an object
+//var student2 = {};
+//student2.firstName = "jack";
+//student2.lastName = "bobo";
+//student2.age = 15;
+//
+//var students = [];
+//students.push(student0);
+//students.push(student1);
+//students.push(student2);
+//
+//console.log(students);
+//
+//
+//
+//
+//
+//
+//
+
+var students = [];
+
+function student(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.greeting = function () {
+        return "I am " + this.firstName + " and I am " + this.age + " years old.";
+    };
 }
 
-var area1 = area(10, 15);
-console.log(area1);
+//var s1 = new student("J", "N", 5);
 
-var rectangles = [];
-rectangles.push(area(10,15));
-rectangles.push(area(13,15));
-rectangles.push(area(10,1));
+//console.log(s1);
+//console.log(s1.greeting());
 
-console.log(rectangles)
+students.push(new student('a', 'b', 20));
+students.push(new student('c', 'd', 24));
 
-
-var bankBalance = 500;
-
-function makeTransaction(priceOfgSale) {
-    if (priceOfgSale <= bankBalance){
-        bankBalance -= priceOfgSale;
-        console.log("Purchase successful")
-    } else {
-        console.log("insufficient funds")
-    }
+for ( var key in students) {
+    console.log(students[key]);
 }
-
-console.log(bankBalance);
-makeTransaction(79.00);
-console.log(bankBalance);
-makeTransaction(9.00);
-console.log(bankBalance);
-makeTransaction(49.00);
-console.log(bankBalance);
-makeTransaction(709.00);
-console.log(bankBalance);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
